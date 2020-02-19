2059d40c75982b6f0be4feb3c0ed73df76be4b23
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-02-19 16:07:03 +0300

    Refactoring floring in getTimeFormatted()

--- js/stopwatchInterface.js
+++ js/stopwatchInterface.js
@@ -33,33 +33,16 @@ export default class StopwatchInterface extends Stopwatch {
         const addZero = val => ((val < 10) ? "0" : "") + val;
         
         const cTime = super.getCurrentTime();
-
-        const hh = addZero(Math.floor(cTime / 3600000));
-        const mm = addZero(Math.floor(cTime % 3600000 / 60000));
-        const ss = addZero(Math.floor(cTime % 3600000 % 60000 / 1000));
-        const cs = addZero(Math.floor(cTime % 1000 / 10));
+        
+        // ^ 0 for flooring
+        const hh = addZero(cTime / 3600000 ^ 0);
+        const mm = addZero(cTime % 3600000 / 60000 ^ 0);
+        const ss = addZero(cTime % 3600000 % 60000 / 1000 ^ 0);
+        const cs = addZero(cTime % 1000 / 10 ^ 0);
         
         const timeStr = `${mm}:${ss}:${cs}`;
         if (hh !== "00") timeStr = `${hh}:${timeStr}`;
 
         return timeStr;
-
-        
-        // const currentTime = new Date(super.getCurrentTime());
-        
-        // const time = {
-        //     hh: currentTime.getUTCHours(),
-        //     mm: currentTime.getUTCMinutes(),
-        //     ss: currentTime.getUTCSeconds(),
-        //     cs: Math.floor(currentTime.getUTCMilliseconds() / 10)
-        // };
-
-        // const t = Object.fromEntries(
-        //     Object.entries(time).map(([key, num]) => [key, `0${num}`.slice(-2)])
-        // );
-
-        // if (time.hh) return `${t.hh}:${t.mm}:${t.ss}:${t.cs}`;
-
-        // return `${t.mm}:${t.ss}:${t.cs}`;
     }
 }
