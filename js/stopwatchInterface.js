fc90347b406d77168b6b9163139c780aba8715b7
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-02-19 15:39:13 +0300

    Refactoring getTimeFormatted() w/o Date object

--- js/stopwatchInterface.js
+++ js/stopwatchInterface.js
@@ -29,21 +29,37 @@ export default class StopwatchInterface extends Stopwatch {
     }
 
     getTimeFormatted() {
-        const currentTime = new Date(super.getCurrentTime());
 
-        const time = {
-            hh: currentTime.getUTCHours(),
-            mm: currentTime.getUTCMinutes(),
-            ss: currentTime.getUTCSeconds(),
-            cs: Math.floor(currentTime.getUTCMilliseconds() / 10)
-        };
+        const addZero = val => ((val < 10) ? "0" : "") + val;
+        
+        const cTime = super.getCurrentTime();
 
-        const t = Object.fromEntries(
-            Object.entries(time).map(([key, num]) => [key, `0${num}`.slice(-2)])
-        );
+        const hh = addZero(Math.floor(cTime / 3600000));
+        const mm = addZero(Math.floor(cTime % 3600000 / 60000));
+        const ss = addZero(Math.floor(cTime % 3600000 % 60000 / 1000));
+        const cs = addZero(Math.floor(cTime % 1000 / 10));
+        
+        const timeStr = `${mm}:${ss}:${cs}`;
+        if (hh !== "00") timeStr = `${hh}:${timeStr}`;
 
-        if (time.hh) return `${t.hh}:${t.mm}:${t.ss}:${t.cs}`;
+        return timeStr;
 
-        return `${t.mm}:${t.ss}:${t.cs}`;
+        
+        // const currentTime = new Date(super.getCurrentTime());
+        
+        // const time = {
+        //     hh: currentTime.getUTCHours(),
+        //     mm: currentTime.getUTCMinutes(),
+        //     ss: currentTime.getUTCSeconds(),
+        //     cs: Math.floor(currentTime.getUTCMilliseconds() / 10)
+        // };
+
+        // const t = Object.fromEntries(
+        //     Object.entries(time).map(([key, num]) => [key, `0${num}`.slice(-2)])
+        // );
+
+        // if (time.hh) return `${t.hh}:${t.mm}:${t.ss}:${t.cs}`;
+
+        // return `${t.mm}:${t.ss}:${t.cs}`;
     }
 }
