865b45e62c005a02a3b806ab5dcd37fab5e9da0a
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-03-09 20:09:33 +0300

    Fix getTimeFormatted()

--- js/stopwatchInterface.js
+++ js/stopwatchInterface.js
@@ -1,48 +1,47 @@
 import Stopwatch from "./stopwatch.js";
 
 export default class StopwatchInterface extends Stopwatch {
-    constructor() {
-        super();
-        this.timerId = 0;
-    }
+  constructor() {
+    super();
+    this.timerId = 0;
+  }
 
-    start(callback, args) {
-        if (this.timerId) return;
-        this.timerId = setInterval(callback, 10, args);
-        super.start();
-    }
+  start(callback, args) {
+    if (this.timerId) return;
+    this.timerId = setInterval(callback, 10, args);
+    super.start();
+  }
 
-    pause() {
-        this.interrupt();
-        super.pause();
-    }
+  pause() {
+    this.interrupt();
+    super.pause();
+  }
 
-    reset(callback, args) {
-        this.interrupt();
-        super.reset();
-        callback(args);
-    }
+  reset(callback, args) {
+    this.interrupt();
+    super.reset();
+    callback(args);
+  }
 
-    interrupt() {
-        clearInterval(this.timerId);
-        this.timerId = 0;
-    }
+  interrupt() {
+    clearInterval(this.timerId);
+    this.timerId = 0;
+  }
 
-    getTimeFormatted() {
+  getTimeFormatted() {
+    const addZero = val => (val < 10 ? "0" : "") + val;
 
-        const addZero = val => ((val < 10) ? "0" : "") + val;
-        
-        const cTime = super.getCurrentTime();
-        
-        // ^ 0 for flooring
-        const hh = addZero(cTime / 3600000 ^ 0);
-        const mm = addZero(cTime % 3600000 / 60000 ^ 0);
-        const ss = addZero(cTime % 3600000 % 60000 / 1000 ^ 0);
-        const cs = addZero(cTime % 1000 / 10 ^ 0);
-        
-        const timeStr = `${mm}:${ss}:${cs}`;
-        if (hh !== "00") timeStr = `${hh}:${timeStr}`;
+    const cTime = super.getCurrentTime();
 
-        return timeStr;
-    }
+    // ^ 0 for flooring
+    const hh = addZero((cTime / 3600000) ^ 0);
+    const mm = addZero(((cTime % 3600000) / 60000) ^ 0);
+    const ss = addZero((((cTime % 3600000) % 60000) / 1000) ^ 0);
+    const cs = addZero(((cTime % 1000) / 10) ^ 0);
+
+    const timeStr = `${mm}:${ss}:${cs}`;
+    if (hh !== "00") timeStr = `${hh}:${timeStr}`;
+
+    return timeStr;
+  }
 }
